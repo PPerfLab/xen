632d360534cb3dbefcf99d5c87e3a6c02e9b08a6
Tejaswini Vibhute <tejaswiniav@gmail.com>
2018-12-13 11:10:12 -0800

    dual_monitor_mode: Updated with Copyright information
    Signed-off-by: Tejaswini Vibhute <tejaswiniav@gmail.com>

--- xen/arch/x86/dual_monitor_mode.c
+++ xen/arch/x86/dual_monitor_mode.c
@@ -5,6 +5,25 @@
  * to protect Xen's critical resources.
  *
  * Tejaswini Vibhute - <tejaswiniav@gmail.com> - Portland State University
+ *
+ * Copyright (C) 2018 Portland State University
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
 #include <asm/dual_monitor_mode.h>
